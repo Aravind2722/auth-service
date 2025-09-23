a1254bdb286dc0af0d1cb6b02cf20f66d4ea235f
Aravind Kumar S <aravindofficial2722@gmail.com>
2025-09-23 20:56:08 +0530

    Primary Changes - Used spring security getting started guide - Used inmemory user and client details to check if everything's in   place

--- src/main/java/org/ecommerce/authservice/configs/SecurityConfig.java
+++ src/main/java/org/ecommerce/authservice/configs/SecurityConfig.java
@@ -1,27 +1,17 @@
 package org.ecommerce.authservice.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().anyRequest().permitAll()
-//                .and().csrf((c) -> c.disable());
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll()
+//                );
 //
 //        return http.build();
 //    }
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
-
-        return http.build();
-    }
 }
